c2e08f256024f6047ea13c469cef77b7ed0786dd
Sarabjeet Singh Hora <sarabjeetxd@gmail.com>
2022-08-13 13:23:03 +0530

    Optimized imports

--- app/src/main/java/com/sarabyeet/toget/ui/fragments/color/CustomColorFragment.kt
+++ app/src/main/java/com/sarabyeet/toget/ui/fragments/color/CustomColorFragment.kt
@@ -2,23 +2,18 @@ package com.sarabyeet.toget.ui.fragments.color
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.snackbar.Snackbar
 import com.sarabyeet.toget.arch.CustomColorViewModel
 import com.sarabyeet.toget.databinding.FragmentCustomColorBinding
 import com.sarabyeet.toget.ui.fragments.BaseFragment
 import com.sarabyeet.toget.util.UserColorsObject
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import java.util.*
 
 class CustomColorFragment : BaseFragment() {
